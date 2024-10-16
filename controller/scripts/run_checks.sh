5019556a79ed7738d3c8ef90998f40986d4792cd
Lukasz Towarek <lukasz.towarek@gmail.com>
2024-10-16 23:43:45 +0200

    Run all checks regardless of exit code

--- controller/scripts/run_checks.sh
+++ controller/scripts/run_checks.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-set -e
-
 SCRIPTPATH=$(dirname "$0")
 
-set -x
-$SCRIPTPATH/run_linters.sh --fix
-$SCRIPTPATH/run_formatter.sh
+STATUS=0
+trap 'STATUS=1' ERR
+
+echo 'run_linters.sh'
+$SCRIPTPATH/run_linters.sh
+echo 'run_formatter.sh'
+$SCRIPTPATH/run_formatter.sh --diff
+echo 'run_type_checks.sh'
 $SCRIPTPATH/run_type_checks.sh
+echo 'run_requirements_checks.sh'
 $SCRIPTPATH/run_requirements_checks.sh
+
+trap - ERR
+
+if [ $STATUS -ne 0 ]
+then
+    echo 'fix_checks.sh'
+    $SCRIPTPATH/fix_checks.sh
+fi
+
+exit $STATUS
