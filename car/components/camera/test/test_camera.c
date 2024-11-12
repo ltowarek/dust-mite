ad5bc72f4181cf3078b1a8d0eb9c626f4cf44dd0
Lukasz Towarek <lukasz.towarek@gmail.com>
2024-11-12 22:03:52 +0000

    Add throughput camera test

--- car/components/camera/test/test_camera.c
+++ car/components/camera/test/test_camera.c
@@ -4,9 +4,22 @@
 #include "esp_timer.h"
 
 
+int is_initialized = 0;
+
+void setUp(void)
+{
+    if (is_initialized == 0) {
+        camera_setup();
+        is_initialized = 1;
+    }
+}
+
+void tearDown(void)
+{
+}
+
 TEST_CASE("performance", "[camera]")
 {
-    camera_setup();
 
     const unsigned MEASUREMENTS = 100;
 
@@ -18,7 +31,34 @@ TEST_CASE("performance", "[camera]")
     uint64_t end = esp_timer_get_time();
 
     uint64_t duration_s = (end-start)/1000000;
+    uint64_t fps = MEASUREMENTS/duration_s;
 
     printf("%u iterations took %llu seconds (%llu frames per second)\n",
-           MEASUREMENTS, duration_s, MEASUREMENTS/duration_s);
+           MEASUREMENTS, duration_s, fps);
+
+    TEST_ASSERT(fps == 25);
+}
+
+
+TEST_CASE("throughput", "[camera]")
+{
+    const unsigned MEASUREMENTS = 100;
+
+    size_t frame_sizes = 0;
+
+    uint64_t start = esp_timer_get_time();
+    for (int i = 0; i < MEASUREMENTS; i++) {
+        camera_fb_t * frame = camera_fb_get();
+        frame_sizes += frame->len;
+        camera_fb_return(frame);
+    }
+    uint64_t end = esp_timer_get_time();
+
+    uint64_t duration_s = (end-start)/1000000;
+    uint64_t throughput_mbps = frame_sizes/duration_s*8/1000000;
+
+    printf("%u iterations took %llu seconds with total frame size of %zu bytes (%llu MBit/s)\n",
+           MEASUREMENTS, duration_s, frame_sizes, throughput_mbps);
+
+    TEST_ASSERT(throughput_mbps == 6);
 }
