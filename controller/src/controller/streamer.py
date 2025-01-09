26b474272923a80857160801eafb52d8e3d65a73
Lukasz Towarek <lukasz.towarek@gmail.com>
2025-01-09 22:20:52 +0000

    Send commands based on telemetry

--- controller/src/controller/streamer.py
+++ controller/src/controller/streamer.py
@@ -1,6 +1,7 @@
 """streamer module."""
 
 import base64
+import contextlib
 import datetime
 import json
 import logging
@@ -9,6 +10,7 @@ import queue
 import threading
 import time
 from types import TracebackType
+from typing import Any
 
 import cv2
 import numpy as np
@@ -23,6 +25,7 @@ logger.setLevel(logging.DEBUG)
 ESP32_ADDRESS = os.environ.get("ESP32_ADDRESS", "ws://192.168.50.66")
 STREAM_CLIENT_URI = ESP32_ADDRESS + "/stream"
 TELEMETRY_CLIENT_URI = ESP32_ADDRESS + "/telemetry"
+CONTROLLER_CLIENT_URI = ESP32_ADDRESS
 
 
 class ClientConnection:
@@ -34,12 +37,14 @@ class ClientConnection:
     All messages that can't be processed will be discarded.
     """
 
-    def __init__(self, uri: str) -> None:
+    def __init__(self, uri: str, mode: str) -> None:
         """Initialize the object."""
         self._uri = uri
+        self._mode = mode
         self._worker = threading.Thread(target=self._worker_main)
         self._stop_event = threading.Event()
-        self._queue: queue.Queue[str | bytes] = queue.Queue(1)
+        self._recv_queue: queue.Queue[str | bytes] = queue.Queue(1)
+        self._send_queue: queue.Queue[str | bytes] = queue.Queue(1)
 
     def _worker_main(self) -> None:
         while not self._stop_event.is_set():
@@ -52,24 +57,47 @@ class ClientConnection:
 
             with client:
                 logger.info("Client connected: %s", self._uri)
+                keep_client_connection = True
 
-                while not self._stop_event.is_set():
-                    try:
-                        frame = client.recv(
-                            timeout=datetime.timedelta(seconds=2).total_seconds(),
-                        )
-                    except websockets.exceptions.ConnectionClosed:
-                        logger.info("%s connection closed", self._uri)
-                        break
-                    except TimeoutError:
-                        logger.warning("%s timed out", self._uri)
-                        break
+                while not self._stop_event.is_set() and keep_client_connection:
+                    # TODO: Rewrite with asyncio
+                    # So "w" and "r" can be supported at the same time
+                    if "w" in self._mode:
+                        try:
+                            self._send_frame(client)
+                        except websockets.exceptions.ConnectionClosed:
+                            logger.info("%s connection closed", self._uri)
+                            keep_client_connection = False
 
-                    try:
-                        self._queue.put_nowait(frame)
-                    except queue.Full:
-                        continue
+                    if "r" in self._mode:
+                        try:
+                            self._recv_frame(client)
+                        except websockets.exceptions.ConnectionClosed:
+                            logger.info("%s connection closed", self._uri)
+                            keep_client_connection = False
+                        except TimeoutError:
+                            logger.warning("%s timed out", self._uri)
+                            keep_client_connection = False
+
         logger.debug("Client worker finished")
+
+    def _send_frame(self, client: websockets.sync.client.ClientConnection) -> None:
+        try:
+            send_frame = self._send_queue.get_nowait()
+        except queue.Empty:
+            return
+
+        try:
+            client.send(send_frame)
+        finally:
+            self._send_queue.task_done()
+
+    def _recv_frame(self, client: websockets.sync.client.ClientConnection) -> None:
+        recv_frame = client.recv(
+            timeout=datetime.timedelta(seconds=2).total_seconds(),
+        )
+        with contextlib.suppress(queue.Full):
+            self._recv_queue.put_nowait(recv_frame)
 
     def connect(self) -> None:
         """Connect to the client."""
@@ -84,11 +112,15 @@ class ClientConnection:
 
     def is_frame_available(self) -> bool:
         """Return `True` if there is a frame ready to be received."""
-        return self._queue.full()
+        return self._recv_queue.full()
 
     def recv(self) -> str | bytes:
         """Receive a frame."""
-        return self._queue.get()
+        return self._recv_queue.get()
+
+    def send(self, frame: str | bytes) -> None:
+        """Send a frame."""
+        self._send_queue.put(frame)
 
     def __enter__(self) -> "ClientConnection":
         """Open a connection in a context manager."""
@@ -110,35 +142,46 @@ def server_handler(websocket: websockets.sync.server.ServerConnection) -> None:
     logger.info("Server connection from: %s", websocket.remote_address[0])
 
     with (
-        ClientConnection(STREAM_CLIENT_URI) as stream_client,
-        ClientConnection(TELEMETRY_CLIENT_URI) as telemetry_client,
+        ClientConnection(STREAM_CLIENT_URI, "r") as stream_client,
+        ClientConnection(TELEMETRY_CLIENT_URI, "r") as telemetry_client,
+        ClientConnection(CONTROLLER_CLIENT_URI, "w") as controller_client,
     ):
-        while True:
+        camera_frame: bytes = b""
+        telemetry: dict[str, Any] = {}
+        keep_server_connection = True
+        while keep_server_connection:
+            packets: list[dict[str, Any]] = []
+            if stream_client.is_frame_available():
+                camera_frame = stream_client.recv()  # type: ignore[assignment]
+                camera_frame = process_frame(camera_frame)
+                packets.append(
+                    {
+                        "type": "stream",
+                        "data": base64.encodebytes(camera_frame).decode("ascii"),
+                    }
+                )
+
             if telemetry_client.is_frame_available():
                 telemetry = json.loads(telemetry_client.recv())
-                packet = {
-                    "type": "telemetry",
-                    "data": telemetry,
-                }
-                try:
-                    websocket.send(json.dumps(packet))
-                except websockets.exceptions.ConnectionClosed:
-                    logger.info("Server connection closed")
-                    break
+                packets.append(
+                    {
+                        "type": "telemetry",
+                        "data": telemetry,
+                    }
+                )
 
-            if stream_client.is_frame_available():
-                camera_frame = stream_client.recv()
-                assert isinstance(camera_frame, bytes)
-                camera_frame = process_frame(camera_frame)
-                packet = {
-                    "type": "stream",
-                    "data": base64.encodebytes(camera_frame).decode("ascii"),
-                }
+            # TODO: Make it opt-in, so user can drive as he wants
+            if packets:
+                command_packet = drive_car(camera_frame, telemetry)
+                if command_packet is not None:
+                    controller_client.send(json.dumps(command_packet))
+
+            for packet in packets:
                 try:
                     websocket.send(json.dumps(packet))
                 except websockets.exceptions.ConnectionClosed:
                     logger.info("Server connection closed")
-                    break
+                    keep_server_connection = False
 
     logger.debug("Server handler finished")
 
@@ -151,6 +194,26 @@ def process_frame(frame: bytes) -> bytes:
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     return cv2.imencode(".jpg", img)[1].tobytes()
+
+
+def drive_car(_frame: bytes, telemetry: dict[str, Any]) -> dict[str, Any] | None:
+    """Update car's state based on camera frame and telemetry."""
+    if not telemetry:
+        return None
+
+    # TODO: Get also current state of the car i.e. commands and their values
+    # If new state is the same as the old state then
+    # there is no need to update the state and send anything
+    # Maybe CONTROLLER_CLIENT_URI should send new state after receiving commands
+
+    min_distance = 5
+    if telemetry["distance_ahead"] < min_distance:
+        # TODO: Integrate with controller.py
+        return {
+            "command": 3,
+            "value": None,
+        }
+    return None
 
 
 if __name__ == "__main__":
