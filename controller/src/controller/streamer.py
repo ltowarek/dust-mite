3db696fea3f97d1ebefe0c522f38ffeca4013171
Lukasz Towarek <lukasz.towarek@gmail.com>
2025-01-09 01:16:32 +0000

    Read telemetry data in streamer

--- controller/src/controller/streamer.py
+++ controller/src/controller/streamer.py
@@ -1,9 +1,14 @@
 """streamer module."""
 
+import base64
 import datetime
+import json
 import logging
 import os
+import queue
+import threading
 import time
+from types import TracebackType
 
 import cv2
 import numpy as np
@@ -16,50 +21,124 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 ESP32_ADDRESS = os.environ.get("ESP32_ADDRESS", "ws://192.168.50.66")
-CLIENT_URI = ESP32_ADDRESS + "/stream"
+STREAM_CLIENT_URI = ESP32_ADDRESS + "/stream"
+TELEMETRY_CLIENT_URI = ESP32_ADDRESS + "/telemetry"
+
+
+class ClientConnection:
+    """Wrapper around Websocket client connection.
+
+    Mostly needed to handle/receive all messages from the client.
+    Even in case when receiver can't process all messages in time.
+
+    All messages that can't be processed will be discarded.
+    """
+
+    def __init__(self, uri: str) -> None:
+        """Initialize the object."""
+        self._uri = uri
+        self._worker = threading.Thread(target=self._worker_main)
+        self._stop_event = threading.Event()
+        self._queue: queue.Queue[str | bytes] = queue.Queue(1)
+
+    def _worker_main(self) -> None:
+        while not self._stop_event.is_set():
+            try:
+                client = websockets.sync.client.connect(self._uri)
+            except (ConnectionRefusedError, OSError, TimeoutError) as e:
+                logger.warning("Failed to connect to the client: %s", e)
+                time.sleep(1)
+                continue
+
+            with client:
+                logger.info("Client connected: %s", self._uri)
+
+                while not self._stop_event.is_set():
+                    try:
+                        frame = client.recv(
+                            timeout=datetime.timedelta(seconds=2).total_seconds(),
+                        )
+                    except websockets.exceptions.ConnectionClosed:
+                        logger.info("%s connection closed", self._uri)
+                        break
+                    except TimeoutError:
+                        logger.warning("%s timed out", self._uri)
+                        break
+
+                    try:
+                        self._queue.put_nowait(frame)
+                    except queue.Full:
+                        continue
+        logger.debug("Client worker finished")
+
+    def connect(self) -> None:
+        """Connect to the client."""
+        logger.debug("Connecting to %s", self._uri)
+        self._worker.start()
+
+    def close(self) -> None:
+        """Connect the connection."""
+        logger.debug("Closing %s connection", self._uri)
+        self._stop_event.set()
+        self._worker.join()
+
+    def is_frame_available(self) -> bool:
+        """Return `True` if there is a frame ready to be received."""
+        return self._queue.full()
+
+    def recv(self) -> str | bytes:
+        """Receive a frame."""
+        return self._queue.get()
+
+    def __enter__(self) -> "ClientConnection":
+        """Open a connection in a context manager."""
+        self.connect()
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        exc_traceback: TracebackType | None,
+    ) -> None:
+        """Close the connection in a context manager."""
+        self.close()
 
 
 def server_handler(websocket: websockets.sync.server.ServerConnection) -> None:
     """WebSocket handler for incoming requests."""
     logger.info("Server connection from: %s", websocket.remote_address[0])
-    keep_server_connection = True
 
-    while keep_server_connection:
-        try:
-            client = websockets.sync.client.connect(CLIENT_URI)
-        except (ConnectionRefusedError, OSError, TimeoutError) as e:
-            logger.warning("Failed to connect to the client: %s", e)
-            time.sleep(1)
-            continue
-
-        with client:
-            logger.info("Client connected: %s", CLIENT_URI)
-            keep_client_connection = True
-
-            while keep_client_connection:
+    with (
+        ClientConnection(STREAM_CLIENT_URI) as stream_client,
+        ClientConnection(TELEMETRY_CLIENT_URI) as telemetry_client,
+    ):
+        while True:
+            if telemetry_client.is_frame_available():
+                telemetry = json.loads(telemetry_client.recv())
+                packet = {
+                    "type": "telemetry",
+                    "data": telemetry,
+                }
                 try:
-                    frame: bytes = client.recv(
-                        timeout=datetime.timedelta(seconds=1).total_seconds(),
-                        decode=False,
-                    )  # type: ignore[assignment]  # `recv(decode=False)` returns bytes
-                except websockets.exceptions.ConnectionClosed:
-                    logger.info("Client connection closed")
-                    keep_client_connection = False
-                    continue
-                except TimeoutError:
-                    logger.warning("Client timed out")
-                    keep_client_connection = False
-                    continue
-
-                frame = process_frame(frame)
-
-                try:
-                    websocket.send(frame)
+                    websocket.send(json.dumps(packet))
                 except websockets.exceptions.ConnectionClosed:
                     logger.info("Server connection closed")
-                    keep_client_connection = False
-                    keep_server_connection = False
-                    continue
+                    break
+
+            if stream_client.is_frame_available():
+                camera_frame = stream_client.recv()
+                assert isinstance(camera_frame, bytes)
+                camera_frame = process_frame(camera_frame)
+                packet = {
+                    "type": "stream",
+                    "data": base64.encodebytes(camera_frame).decode("ascii"),
+                }
+                try:
+                    websocket.send(json.dumps(packet))
+                except websockets.exceptions.ConnectionClosed:
+                    logger.info("Server connection closed")
+                    break
 
     logger.debug("Server handler finished")
 
--- controller/src/controller/web/index.js
+++ controller/src/controller/web/index.js
@@ -1,32 +1,31 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const stream_socket = new WebSocket("ws://localhost:8765");
-  stream_socket.binaryType = "arraybuffer";
+  const socket = new WebSocket("ws://localhost:8765");
 
-  stream_socket.onmessage = (event) => {
-    const array_u8 = new Uint8Array(event.data);
-    image.src = "data:image/jpeg;base64," + window.btoa(String.fromCharCode.apply(null, array_u8));
+  socket.onmessage = (event) => {
+    const event_data = JSON.parse(event.data);
+
+    if (event_data["type"] == "stream") {
+      data = event_data["data"];
+      image.src = "data:image/jpeg;base64," + data;
+    } else if (event_data["type"] == "telemetry") {
+      data = event_data["data"];
+
+      timestamp.innerText = data.timestamp;
+      rssi.innerText = `${data.rssi} dBm`;
+      speed.innerText = `${data.speed.toFixed(2)} km/h`;
+
+      const a = data.accelerometer;
+      const m = data.magnetometer;
+      const g = data.gyroscope;
+      accelerometer.innerText = `${a.x.toFixed(2)}, ${a.y.toFixed(2)}, ${a.z.toFixed(2)} g`;
+      magnetometer.innerText = `${m.x.toFixed(2)}, ${m.y.toFixed(2)}, ${m.z.toFixed(2)} G`;
+      gyroscope.innerText = `${g.x.toFixed(2)}, ${g.y.toFixed(2)}, ${g.z.toFixed(2)} degrees/s`;
+
+      distance_ahead.innerText = `${data.distance_ahead} cm`;
+    }
   };
 
   const image = document.getElementById("image");
-
-  const telemetry_socket = new WebSocket("ws://<ESP32_ADDRESS>/telemetry");
-
-  telemetry_socket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    timestamp.innerText = data.timestamp;
-    rssi.innerText = `${data.rssi} dBm`;
-    speed.innerText = `${data.speed.toFixed(2)} km/h`;
-
-    const a = data.accelerometer;
-    const m = data.magnetometer;
-    const g = data.gyroscope;
-    accelerometer.innerText = `${a.x.toFixed(2)}, ${a.y.toFixed(2)}, ${a.z.toFixed(2)} g`;
-    magnetometer.innerText = `${m.x.toFixed(2)}, ${m.y.toFixed(2)}, ${m.z.toFixed(2)} G`;
-    gyroscope.innerText = `${g.x.toFixed(2)}, ${g.y.toFixed(2)}, ${g.z.toFixed(2)} degrees/s`;
-
-    distance_ahead.innerText = `${data.distance_ahead} cm`;
-  };
-
   const timestamp = document.getElementById("timestamp");
   const rssi = document.getElementById("rssi");
   const speed = document.getElementById("speed");
